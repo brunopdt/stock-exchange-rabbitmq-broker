363b50670032341aa1c901b2dc28fd49d3678bc2
bruno duarte <bruno.pduarte0@gmail.com>
2024-05-05 15:45:55 -0300

    feat: buy & sell api routes (conflict solving)

--- src/main/java/com/group1/stockexchange/controllers/TransactionController.java
+++ src/main/java/com/group1/stockexchange/controllers/TransactionController.java
@@ -1,13 +1,13 @@
 package com.group1.stockexchange.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.group1.stockexchange.models.OrderDTO;
 import com.group1.stockexchange.models.TransactionModel;
 import com.group1.stockexchange.services.TransactionService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +17,32 @@ import java.util.List;
 public class TransactionController {
     @Autowired
     private TransactionService transactionService;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @PostMapping("/sell")
+    public String sell(@RequestBody OrderDTO orderDTO) {
+        return sendOrder(orderDTO, "sell");
+    }
+
+    @PostMapping("/buy")
+    public String buy(@RequestBody OrderDTO orderDTO) {
+        return sendOrder(orderDTO, "buy");
+    }
+
+    private String sendOrder(OrderDTO orderDTO, String orderType) {
+        try {
+            String jsonOrder = objectMapper.writeValueAsString(orderDTO);
+            rabbitTemplate.convertAndSend(orderType, orderDTO.getActive(), jsonOrder.getBytes());
+            return "[X] " + orderType.substring(0,1).toUpperCase() + orderType.substring(1) + " order sent successfully!";
+        } catch (Exception e) {
+            return "Error: " + e.getMessage();
+        }
+    }
 
     @GetMapping("/broker/{id}")
     public List<TransactionModel> getBrokerTransactions(@PathVariable Long id) {
